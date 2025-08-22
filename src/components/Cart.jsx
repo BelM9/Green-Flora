6ca0c06deae9b86e36056f0f5fc95d95e61a5da0
Moez <bellahirechmoez@gmail.com>
2025-08-22 16:50:09 +0100

    Commit3 de site web Green-flora

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -5,15 +5,15 @@ function Cart() {
     const ivyPrice = 10
     const flowerPrice = 15
     return (<div>
-        <h2>Panier</h2>
+        {/*h2+(ul>li*3)+p*/}
+        <h2></h2>
         <ul>
-            <li>Monstera : {monsteraPrice}d</li>
-            <li>Lierre : {ivyPrice}d</li>
-            <li>Fleurs : {flowerPrice}d</li>
+            <li>Monstera : {monsteraPrice} d</li>
+            <li>Lierre   : {ivyPrice} d</li>
+            <li>Fleurs   : {flowerPrice} d</li>
         </ul>
-        Total : {monsteraPrice + ivyPrice + flowerPrice}d
+        <p>Total : {monsteraPrice + ivyPrice + flowerPrice} d</p>
+
     </div>)
-
 }
-
 export default Cart
