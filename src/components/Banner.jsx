a57e6c00379f47587327b6f296a0bf324922b9cb
Moez <bellahirechmoez@gmail.com>
2025-08-23 08:36:51 +0100

    Commit7 de site web Green-flora

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,14 +1,14 @@
 //rfce - ES7 snippet
 import React from 'react'
+import '../styles/banner.css'
+import logo from '../assets/logo.png'
 
 function Banner() {
-    const titre = "Green Flora"
-
+    const title = 'Green Flora'
     return (
-        <div>
-            {/*<h1>Green Flora</h1>*/}
-
-            <h1>{titre}</h1>
+        <div className='gf-banner'>
+            <img src={logo} alt={title} className='gf_logo' />
+            <h1 h1 className='gf-title'>{title}</h1>
         </div>
     )
 }
