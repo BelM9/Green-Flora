c643dbcbd72718c92dc4f4c5e44b353e10115c60
Moez <bellahirechmoez@gmail.com>
2025-08-27 15:57:55 +0100

    Commit13 de site web Green-flora

--- src/components/ShoppingList.jsx
+++ src/components/ShoppingList.jsx
@@ -1,79 +1,38 @@
 import React from 'react'
 import { plantList } from '../datas/plantList'
-
-// // const plantList = [
-// //     'monstera',
-// //     'ficus lyrata',
-// //     'pothos argenté',
-// //     'yucca',
-// //     'palmier'
-// // ]
-
-// function ShoppingList() {
-//     const categories = plantListDatas.reduce(
-// 		(acc, plant) =>
-// 			acc.includes(plant.category) ? acc : acc.concat(plant.category),
-// 		[]
-// 	)
-//     return (
-//         <div>
-//             <ul>
-
-//                 {/* { plantList.map( (plant) => (<li>{plant}</li>) ) 
-//                 {   // utilisons key = index
-//                     planList.map((plant, index) => (<li key={`${plant}-${index}`}>{plant}</li>))
-//                 } */}
-
-//                 {categories.map((cat) => (
-// 					<li key={cat}>{cat}</li>
-// 				))}
-//             </ul>
-//             <ul>
-// 				{plantListDatas.map((plant) => (
-// 					<li key={plant.id}>{plant.name}</li>
-// 				))}
-// 			</ul>
-//         </div >
-//     )
-// }
-
-// export default ShoppingList
+import '../styles/ShoppingList.css'
 
 function ShoppingList() {
-//     const categories = []
-//     plantList.map((plant) => {
-//         categories.forEach((plant.category) => {
-//             if(!categories.includes(plant.category)) {
-//             categories.push(plant.category)
-//         }
-//     })
-// }
 
-const categories = Array.from(new Set(plantList.map(plant => plant.category)));
+    const categories = Array.from(new Set(plantList.map(plant => plant.category)));
 
-return (
-    <>
-        <div>
-            <h2>Catégories</h2>
-            <ul>
-                {categories.map((category) => (
-                    <li key={category}>{category}</li>
-                ))}
-            </ul>
+    return (
+        <div className='gf-container'>
+            <div>
+                <h2>Catégories</h2>
+                <ul>
+                    {categories.map((categ) => (
+                        <li key={categ}>{categ}</li>
+                    ))}
+                </ul>
+            </div>
+
+            <hr />
+
+            <div>
+                <h2>Plantes</h2>
+                <ul className='gf-plant-list'>
+                    {plantList.map((plant) => (
+                        <li key={plant.id} className='gf-plant-item'>
+                            {plant.name}
+                            {plant.isSpecialOffer && <div className='gf-sales'>Solde</div>}
+                        </li>
+                    ))}
+                </ul>
+            </div>
         </div>
-
-        <hr />
-
-        <div>
-            <h2>Plantes</h2>
-            <ul>
-                {plantList.map((plant) => (
-                    <li key={plant.id}>{plant.name}</li>
-                ))}
-            </ul>
-        </div>
-    </>
-)
+    )
 }
 
 export default ShoppingList
+
--- src/datas/plantList.js
+++ src/datas/plantList.js
@@ -1,57 +1,50 @@
-
 export const plantList = [
-    {
-        id: '1ed',
-        name: 'monstera',
-        category: 'classique'
-
-    },
-    {
-        id: '2ab',
-        name: 'ficus lyrata',
-        category: 'classique'
-
-    },
-    {
-        id: '3sd',
-        name: 'pothos argenté',
-        category: 'classique'
-
-    },
-    {
-        id: '4kk',
-        name: 'yucca',
-        category: 'classique'
-
-    },
-    {
-        id: '5pl',
-        name: 'olivier',
-        category: 'extérieur'
-
-    },
-    {
-        id: '6uo',
-        name: 'géranium',
-        category: 'extérieur'
-
-    },
-    {
-        id: '7ie',
-        name: 'basilique',
-        category: 'extérieur'
-
-    },
-    {
-        id: '8fp',
-        name: 'aloe',
-        category: 'plante grasse'
-
-    },
-    {
-        id: '9vn',
-        name: 'succulente',
-        category: 'plante grasse'
-
-    }
+	{
+		name: 'monstera',
+		category: 'classique',
+		id: '1ed'
+	},
+	{
+		name: 'ficus lyrata',
+		category: 'classique',
+		id: '2ab'
+	},
+	{
+		name: 'pothos argenté',
+		category: 'classique',
+		id: '3sd',
+		isSpecialOffer: true
+	},
+	{
+		name: 'yucca',
+		category: 'classique',
+		id: '4kk'
+	},
+	{
+		name: 'olivier',
+		category: 'extérieur',
+		id: '5pl'
+	},
+	{
+		name: 'géranium',
+		category: 'extérieur',
+		id: '6uo'
+	},
+	{
+		name: 'basilique',
+		category: 'extérieur',
+		id: '7ie',
+		isSpecialOffer: true
+	},
+	{
+		name: 'aloe',
+		category: 'plante grasse',
+		id: '8fp',
+		isSpecialOffer: true
+	},
+	{
+		name: 'succulente',
+		category: 'plante grasse',
+		id: '9vn'
+	}
 ]
